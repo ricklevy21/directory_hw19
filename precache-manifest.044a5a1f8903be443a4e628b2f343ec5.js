08d87f534859ffcdaa6c079c552d52494b156804
Rick Levy <ricklevy21@gmail.com>
2020-08-15 14:14:18 -0600

    Updates

---
+++ precache-manifest.044a5a1f8903be443a4e628b2f343ec5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d4bd756c1851d8e4813f232dc7121e1",
+    "url": "/directory_hw19/index.html"
+  },
+  {
+    "revision": "f51d8061d02d029d1d7c",
+    "url": "/directory_hw19/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f2510ba36e68dbfb875b",
+    "url": "/directory_hw19/static/js/2.61ea89ec.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/directory_hw19/static/js/2.61ea89ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f51d8061d02d029d1d7c",
+    "url": "/directory_hw19/static/js/main.998d441c.chunk.js"
+  },
+  {
+    "revision": "983d82943eea29871d8d",
+    "url": "/directory_hw19/static/js/runtime-main.4690196b.js"
+  }
+]);
